53b4bbd2299789a7cd510daabd9b3b1ae6e92f27
Jorge Mendoza <jorgemendoza@MacBook-Pro.local>
2026-02-03 22:24:55 -0600

    Remove scroll indicator from Hero section

--- barber-blueprint/src/components/Hero.jsx
+++ barber-blueprint/src/components/Hero.jsx
@@ -126,21 +126,6 @@ export default function Hero() {
         </motion.div>
       </div>
 
-      {/* Scroll Indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-      >
-        <div className="w-6 h-10 border border-white/20 rounded-full flex justify-center">
-          <motion.div
-            animate={{ y: [0, 12, 0] }}
-            transition={{ duration: 1.5, repeat: Infinity }}
-            className="w-1 h-1 bg-white/40 rounded-full mt-2"
-          />
-        </div>
-      </motion.div>
     </section>
   )
 }
